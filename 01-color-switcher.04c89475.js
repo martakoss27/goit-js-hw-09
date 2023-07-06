ac133200f1d8f3271f65ba0092036a9f404847cb
martakoss27 <115739746+martakoss27@users.noreply.github.com>
2023-07-06 11:34:52 +0000

    Deploying to gh-pages from @ martakoss27/goit-js-hw-09@b07aafe67df4f36b54dec02ab7a14fac973cf49a 🚀

---
+++ 01-color-switcher.04c89475.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),o=null;t.addEventListener("click",function(){o=setInterval(function(t){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))},1e3)}),n.addEventListener("click",function(){clearInterval(o),console.log("Interval with id ".concat(o," has stopped!"))})}();
+//# sourceMappingURL=01-color-switcher.04c89475.js.map
---
+++ 01-color-switcher.b16bc8a3.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let o=null;t.addEventListener("click",()=>{o=setInterval(t=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`},1e3)}),e.addEventListener("click",()=>{clearInterval(o),console.log(`Interval with id ${o} has stopped!`)});
+//# sourceMappingURL=01-color-switcher.b16bc8a3.js.map
