b193c43be79fd6b05fa824b2e72767fce64e3ecd
MartaKoss27 <27691497m@gmail.com>
2023-08-28 19:40:38 +0200

    Update zad 1 hw js 09

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,20 +1,26 @@
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 let timerId = null;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+stopBtn.disabled = true;
 
 startBtn.addEventListener('click', () => {
   timerId = setInterval(event => {
     const currentColor = (document.body.style.backgroundColor =
       getRandomHexColor());
   }, 1000);
+  stopBtn.disabled = false;
+  startBtn.disabled = true;
 });
 
 stopBtn.addEventListener('click', () => {
   clearInterval(timerId);
   console.log(`Interval has stopped!`);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
 });
